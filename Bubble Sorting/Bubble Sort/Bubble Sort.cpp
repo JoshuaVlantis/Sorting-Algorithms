a82b822feb6fdcb190a04ee92dd3e47a3b518595
Joshua Vlantis <70960513+JoshuaVlantis@users.noreply.github.com>
2021-08-19 11:01:46 +0200

    Updates

--- Bubble Sorting/Bubble Sort/Bubble Sort.cpp
+++ Bubble Sorting/Bubble Sort/Bubble Sort.cpp
@@ -7,12 +7,12 @@ int main()
     std::ifstream file("Arrayfile.txt");
     if (file.is_open())
     {
-        int sortingArray[10];
+        int sortingArray[100];
         int tempArray[2];
         int h = 1;
         bool loopagain = false;
 
-        for (int p = 0; p < 10; ++p)
+        for (int p = 0; p < 100; ++p)
         {
             file >> sortingArray[p];
         }
@@ -20,13 +20,12 @@ int main()
         for (int i = 0; i < h; i++)
         {
 
-            for (int ii = 0; ii < 9; ii++)
+            for (int ii = 0; ii < 99; ii++)
             {
                 if (sortingArray[ii] > sortingArray[ii + 1])
                 {
                     tempArray[0] = sortingArray[ii];
-                    tempArray[1] = sortingArray[ii + 1];
-                    sortingArray[ii] = tempArray[1];
+                    sortingArray[ii] = sortingArray[ii + 1];
                     sortingArray[ii + 1] = tempArray[0];
                     loopagain = true;
                 }
@@ -38,10 +37,11 @@ int main()
             loopagain = false;
 
         }
-        for (int k = 0; k < 10; k++)
+        for (int k = 0; k < 100; k++)
         {
             std::cout << sortingArray[k] << "\n";
         }
+        
         std::cout << "\n";
         std::cout << "Array was sorted in " << h << " passes\n";
     }
@@ -49,5 +49,4 @@ int main()
     {
         std::cout << "file not found \n";
     }
-
 }
--- Selection Sorting/Selection Sorting/Selection Sorting.cpp
+++ Selection Sorting/Selection Sorting/Selection Sorting.cpp
@@ -7,26 +7,25 @@ int main()
     std::ifstream file("Arrayfile.txt");
     if (file.is_open())
     {
-        int sortingArray[10];
+        int sortingArray[100];
         int tempArray[2];
-        for (int p = 0; p < 10; ++p)
+        for (int p = 0; p < 100; ++p)
         {
             file >> sortingArray[p];
         }
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 100; i++)
         {
-            for (int ii = 0; ii < 10; ii++)
+            for (int ii = 0; ii < 100; ii++)
             {
                 if (sortingArray[i] < sortingArray[ii])
                 {
                     tempArray[0] = sortingArray[i];
-                    tempArray[1] = sortingArray[ii];
-                    sortingArray[i] = tempArray[1];
+                    sortingArray[i] = sortingArray[ii];
                     sortingArray[ii] = tempArray[0];
                 }
             }
         }
-        for (int k = 0; k < 10; k++)
+        for (int k = 0; k < 100; k++)
         {
             std::cout << sortingArray[k] << "\n";
         }
