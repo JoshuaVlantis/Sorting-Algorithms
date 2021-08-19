717a8af7730e87e0db544210b257e4b44a7533c5
Joshua <70960513+JoshuaVlantis@users.noreply.github.com>
2021-08-19 15:47:30 +0200

    Added timer
    Added timer to work out how long it took for the program to sort the array.

--- Bubble Sorting/Bubble Sort/Bubble Sort.cpp
+++ Bubble Sorting/Bubble Sort/Bubble Sort.cpp
@@ -2,19 +2,19 @@
 #include <string>
 #include <fstream>
 #include <chrono>
-#include <thread>
+
+int sortingArray[5000];
+int output[2];
 
 void sort()
 {
-
     using namespace std::chrono_literals;
-
+   
     std::ifstream file("Arrayfile.txt");
     if (file.is_open())
     {
-        int sortingArray[5000];
-        int tempArray[2];
         int h = 1;
+        int tempArray[1];
         bool loopagain = false;
 
         for (int p = 0; p < 5000; ++p)
@@ -24,7 +24,6 @@ void sort()
 
         for (int i = 0; i < h; i++)
         {
-
             for (int ii = 0; ii < 4999; ii++)
             {
                 if (sortingArray[ii] > sortingArray[ii + 1])
@@ -40,15 +39,8 @@ void sort()
                 h++;
             }
             loopagain = false;
-
         }
-        //for (int k = 0; k < 5000; k++)
-        //{
-        //    std::cout << sortingArray[k] << "\n";
-        //}
-
-        std::cout << "\n";
-        std::cout << "Array was sorted in " << h << " passes\n";
+        output[0] = h;
     }
     else
     {
@@ -58,6 +50,7 @@ void sort()
 
 int main()
 {
+    int showoutput;
     using std::chrono::high_resolution_clock;
     using std::chrono::duration_cast;
     using std::chrono::duration;
@@ -67,13 +60,26 @@ int main()
     sort();
     auto t2 = high_resolution_clock::now();
 
-    /* Getting number of milliseconds as an integer. */
+    //Getting number of milliseconds as an integer
     auto ms_int = duration_cast<milliseconds>(t2 - t1);
-
-    /* Getting number of milliseconds as a double. */
+    //Getting number of milliseconds as a double
     duration<double, std::milli> ms_double = t2 - t1;
 
-    std::cout << ms_int.count() << "ms\n";
-    std::cout << ms_double.count() << "ms";
+    std::cout << "\n";
+    std::cout << "Array has been sorted in " << output[0] << " passes in " << ms_double.count() << "ms\n";
+    //std::cout << ms_double.count() << "ms";
+    std::cout << "\n";
+    std::cout << "Would you like to show output\n";
+    std::cout << "[1] Yes                [2] No\n";
+    std::cin >> showoutput;
+    
+    if (showoutput == 1)
+    {
+        //Output sorted Array
+        for (int k = 0; k < 5000; k++)
+        {
+            std::cout << sortingArray[k] << "\n";
+        }
+    }
     return 0;
 }
