799f305003be0909ebe3ab137e6d4f7b782a3abc
Joshua Vlantis <70960513+JoshuaVlantis@users.noreply.github.com>
2021-08-21 13:21:32 +0200

    Loaded file before timer starts

--- Bubble Sorting/Bubble Sort/Bubble Sort.cpp
+++ Bubble Sorting/Bubble Sort/Bubble Sort.cpp
@@ -9,77 +9,78 @@ int output[2];
 void sort()
 {
     using namespace std::chrono_literals;
-   
-    std::ifstream file("Arrayfile.txt");
-    if (file.is_open())
-    {
-        int h = 1;
-        int tempArray[1];
-        bool loopagain = false;
+     
+    int h = 1;
+    int tempArray[1];
+    bool loopagain = false;
 
-        for (int p = 0; p < 5000; ++p)
-        {
-            file >> sortingArray[p];
-        }
-
-        for (int i = 0; i < h; i++)
-        {
-            for (int ii = 0; ii < 4999; ii++)
-            {
-                if (sortingArray[ii] > sortingArray[ii + 1])
-                {
-                    tempArray[0] = sortingArray[ii];
-                    sortingArray[ii] = sortingArray[ii + 1];
-                    sortingArray[ii + 1] = tempArray[0];
-                    loopagain = true;
-                }
-            }
-            if (loopagain != false)
-            {
-                h++;
-            }
-            loopagain = false;
-        }
-        output[0] = h;
-    }
-    else
+    for (int i = 0; i < h; i++)
     {
-        std::cout << "file not found \n";
+        for (int ii = 0; ii < 4999; ii++)
+        {
+            if (sortingArray[ii] > sortingArray[ii + 1])
+            {
+                tempArray[0] = sortingArray[ii];
+                sortingArray[ii] = sortingArray[ii + 1];
+                sortingArray[ii + 1] = tempArray[0];
+                loopagain = true;
+            }
+        }
+        if (loopagain != false)
+        {
+            h++;
+        }
+        loopagain = false;
     }
+    output[0] = h;
 }
 
 int main()
 {
-    int showoutput;
-    using std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::duration;
-    using std::chrono::milliseconds;
-
-    auto t1 = high_resolution_clock::now();
-    sort();
-    auto t2 = high_resolution_clock::now();
-
-    //Getting number of milliseconds as an integer
-    auto ms_int = duration_cast<milliseconds>(t2 - t1);
-    //Getting number of milliseconds as a double
-    duration<double, std::milli> ms_double = t2 - t1;
-
-    std::cout << "\n";
-    std::cout << "Array has been sorted in " << output[0] << " passes in " << ms_double.count() << "ms\n";
-    //std::cout << ms_double.count() << "ms";
-    std::cout << "\n";
-    std::cout << "Would you like to show output\n";
-    std::cout << "[1] Yes                [2] No\n";
-    std::cin >> showoutput;
-    
-    if (showoutput == 1)
+    std::ifstream file("Arrayfile.txt");
+    if (file.is_open())
     {
-        //Output sorted Array
-        for (int k = 0; k < 5000; k++)
+        for (int p = 0; p < 5000; ++p)
         {
-            std::cout << sortingArray[k] << "\n";
+            file >> sortingArray[p];
         }
+        
+        int showoutput;
+        using std::chrono::high_resolution_clock;
+        using std::chrono::duration_cast;
+        using std::chrono::duration;
+        using std::chrono::milliseconds;
+
+        auto t1 = high_resolution_clock::now();
+        sort();
+        auto t2 = high_resolution_clock::now();
+
+        //Getting number of milliseconds as an integer
+        auto ms_int = duration_cast<milliseconds>(t2 - t1);
+        //Getting number of milliseconds as a double
+        duration<double, std::milli> ms_double = t2 - t1;
+
+        std::cout << "Bubble Sort\n";
+        std::cout << "\n";
+        std::cout << "Array has been sorted in " << output[0] << " passes in " << ms_double.count() << "ms\n";
+        //std::cout << ms_double.count() << "ms";
+        std::cout << "\n";
+        std::cout << "Would you like to show output\n";
+        std::cout << "[1] Yes                [2] No\n";
+        std::cin >> showoutput;
+        
+        if (showoutput == 1)
+        {
+            //Output sorted Array
+            for (int k = 0; k < 5000; k++)
+            {
+                std::cout << sortingArray[k] << "\n";
+            }
+        }       
+    }
+    else
+    {
+        std::cout << "file not found \n";
     }
     return 0;
 }
--- Selection Sorting/Selection Sorting/Selection Sorting.cpp
+++ Selection Sorting/Selection Sorting/Selection Sorting.cpp
@@ -4,73 +4,74 @@
 #include <chrono>
 
 int sortingArray[5000];
-int output[2];
+int output[1];
 
 void sort()
 {
-    std::ifstream file("Arrayfile.txt");
-    if (file.is_open())
+    int h = 1;
+    int tempArray[1];
+       
+    for (int i = 0; i < 5000; i++)
     {
-        int h = 1;
-        int tempArray[1];
-        for (int p = 0; p < 5000; ++p)
+        for (int ii = 0; ii < 5000; ii++)
         {
-            file >> sortingArray[p];
-        }
-        for (int i = 0; i < 5000; i++)
-        {
-            for (int ii = 0; ii < 5000; ii++)
+            if (sortingArray[i] < sortingArray[ii])
             {
-                if (sortingArray[i] < sortingArray[ii])
-                {
-                    tempArray[0] = sortingArray[i];
-                    sortingArray[i] = sortingArray[ii];
-                    sortingArray[ii] = tempArray[0];
-                }
+                tempArray[0] = sortingArray[i];
+                sortingArray[i] = sortingArray[ii];
+                sortingArray[ii] = tempArray[0];
             }
-            h++;
-            output[0] = h;
         }
-    }
-    else
-    {
-        std::cout << "file not found \n";
+        h++;
+        output[0] = h;
     }
 }
 
 int main()
 {
-    int showoutput;
-    using std::chrono::high_resolution_clock;
-    using std::chrono::duration_cast;
-    using std::chrono::duration;
-    using std::chrono::milliseconds;
-
-    auto t1 = high_resolution_clock::now();
-    sort();
-    auto t2 = high_resolution_clock::now();
-
-    //Getting number of milliseconds as an integer
-    auto ms_int = duration_cast<milliseconds>(t2 - t1);
-    //Getting number of milliseconds as a double
-    duration<double, std::milli> ms_double = t2 - t1;
-
-    std::cout << "\n";
-    std::cout << "Array has been sorted in " << output[0] << " passes in " << ms_double.count() << "ms\n";
-    //std::cout << ms_double.count() << "ms";
-    std::cout << "\n";
-    std::cout << "Selection Sorting";
-    std::cout << "Would you like to show output\n";
-    std::cout << "[1] Yes                [2] No\n";
-    std::cin >> showoutput;
-
-    if (showoutput == 1)
+    std::ifstream file("Arrayfile.txt");
+    if (file.is_open())
     {
-        //Output sorted Array
-        for (int k = 0; k < 5000; k++)
+        for (int p = 0; p < 5000; ++p)
         {
-            std::cout << sortingArray[k] << "\n";
+            file >> sortingArray[p];
         }
+        int showoutput;
+        using std::chrono::high_resolution_clock;
+        using std::chrono::duration_cast;
+        using std::chrono::duration;
+        using std::chrono::milliseconds;
+
+        auto t1 = high_resolution_clock::now();
+        sort();
+        auto t2 = high_resolution_clock::now();
+
+        //Getting number of milliseconds as an integer
+        auto ms_int = duration_cast<milliseconds>(t2 - t1);
+        //Getting number of milliseconds as a double
+        duration<double, std::milli> ms_double = t2 - t1;
+        
+        std::cout << "Selection Sorting\n";
+        std::cout << "\n";
+        std::cout << "Array has been sorted in " << output[0] << " passes in " << ms_double.count() << "ms\n";
+        //std::cout << ms_double.count() << "ms";
+        std::cout << "\n";
+        std::cout << "Would you like to show output\n";
+        std::cout << "[1] Yes                [2] No\n";
+        std::cin >> showoutput;
+
+        if (showoutput == 1)
+        {
+            //Output sorted Array
+            for (int k = 0; k < 5000; k++)
+            {
+            std::cout << sortingArray[k] << "\n";
+            }
+        }
+    }
+    else
+    {
+        std::cout << "file not found \n";
     }
     return 0;
 }
